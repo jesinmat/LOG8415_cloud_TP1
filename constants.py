4d1c49f7d2ed9817436ca923c1737f8ba54e3e75
Pierre Ballif <pierre.ballif@tum.de>
2021-10-10 20:09:42 -0400

    Use my branch and my constants

---
+++ constants.py
@@ -0,0 +1,5 @@
+def KEYPAIR_NAME():
+	return '../amazon-key.pem'
+
+def SECURITY_GROUP():
+	return input('Please enter a security group name.')
---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+git clone git@github.com:jesinmat/LOG8415_cloud_TP1.git && cd LOG8415_cloud_TP1
+git checkout pierre
+
+python3 - <<'END_SCRIPT'
+
+import subprocess
+from load_balancer import manager
+
+manager = AmazonManager()
+manager.setup()
+
+subprocess.run(f"""#!/usr/bin/bash
+cd docker-loadtester
+./buildDockerImage.sh
+
+export AWS_URL='http://{manager.dns_name}'
+./runDockerContainer.sh
+""")
+
+manager.shutdown()
+
+END_SCRIPT
