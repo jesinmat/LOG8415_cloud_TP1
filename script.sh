9a7e12d1fdae12884c8dffb10b18cbdd5ed4e2cf
Pierre Ballif <pierre.ballif@tum.de>
2021-10-10 22:45:31 -0400

    Do not delete the Git folder

--- script.sh
+++ script.sh
@@ -7,23 +7,23 @@ python3 - <<'END_SCRIPT'
 
 import subprocess
 import os
+import time
 from load_balancer import AmazonManager
 
 manager = AmazonManager()
 manager.setup()
 
+time.sleep(60)
+
 completed = subprocess.run(["bash", "-c",
 f"""#!/usr/bin/bash
-cd docker-loadtester
-./buildDockerImage.sh
+cd docker-loadtester &&
+./buildDockerImage.sh &&
 
-export AWS_URL='http://{manager.dns_name}'
+export AWS_URL='http://{manager.dns_name}' &&
 ./runDockerContainer.sh
 """])
 
 manager.shutdown()
 
 END_SCRIPT
-
-cd ..
-rm LOG8415_cloud_TP1 -rf
